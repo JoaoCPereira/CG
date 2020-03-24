610e61b883fa85b14d5553122b58b89fdb637b00
Gulithor <manuel.a.m.marques.1999@gmail.com>
2020-03-24 13:25:16 +0000

    Scales

---
+++ Projecto/CMakeFiles/Engine.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gulithor/LCC/3ano/CG/CG/Projecto/main.cpp" "/home/gulithor/LCC/3ano/CG/CG/Projecto/CMakeFiles/Engine.dir/main.cpp.o"
+  "/home/gulithor/LCC/3ano/CG/CG/Projecto/readXML/readXMLAUX.cpp" "/home/gulithor/LCC/3ano/CG/CG/Projecto/CMakeFiles/Engine.dir/readXML/readXMLAUX.cpp.o"
+  "/home/gulithor/LCC/3ano/CG/CG/Projecto/readXML/tinyXML/tinystr.cpp" "/home/gulithor/LCC/3ano/CG/CG/Projecto/CMakeFiles/Engine.dir/readXML/tinyXML/tinystr.cpp.o"
+  "/home/gulithor/LCC/3ano/CG/CG/Projecto/readXML/tinyXML/tinyxml.cpp" "/home/gulithor/LCC/3ano/CG/CG/Projecto/CMakeFiles/Engine.dir/readXML/tinyXML/tinyxml.cpp.o"
+  "/home/gulithor/LCC/3ano/CG/CG/Projecto/readXML/tinyXML/tinyxmlerror.cpp" "/home/gulithor/LCC/3ano/CG/CG/Projecto/CMakeFiles/Engine.dir/readXML/tinyXML/tinyxmlerror.cpp.o"
+  "/home/gulithor/LCC/3ano/CG/CG/Projecto/readXML/tinyXML/tinyxmlparser.cpp" "/home/gulithor/LCC/3ano/CG/CG/Projecto/CMakeFiles/Engine.dir/readXML/tinyXML/tinyxmlparser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "readXML/tinyXML"
+  "readXML"
+  "generatorSrc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
